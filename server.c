c3059828597d8ac8cbf3d65918010ec8f641c2c7
Douglas Poerschke Rocha <douglas.poerschke@gmail.com>
2022-04-01 18:17:34 -0300

    Update server.c

--- server.c
+++ server.c
@@ -14,26 +14,13 @@
 #include <netinet/tcp.h>
 #include <netdb.h>
 #include <pthread.h>
-#include "queue.h"
 #include <mysql.h>
 
 #define BUFFER_SIZE 1024 * 1024
-#define THREADS 20
-#define TIMEOUT 2
+#define MAX_TH 30
 
-const int  queue_s = 100000;
-
-
-
-
-void parser2(char *string);
-int socket_connect(char *host, in_port_t port);
-int split(char *str, char *id);
-void head(char *host, int port, char *server);
-void *worker(void *arg);
-void grava(char arquivo[], char ip[], int porta, char server[]);
-
-
+pthread_mutex_t lock;
+pthread_cond_t cond;
 
 char *server = "localhost";
 char *user = "root";
@@ -41,286 +28,269 @@ char *password = ""; /* set me first */
 char *database = "dados";
 
 
+void InitPilha();
+int pilhaVazia();
+void empilha(char ip[30], int porta, int id);
+void desempilha(char *ip, int *porta, int *id);
+void * worker();
+void parser2(char *string);
+int socket_connect(char *host, in_port_t port);
+void head(char *host, int port, char *banner, char *headers);
+void removeChar(char * str, char charToRemmove);
+
+struct node {
+    int id;
+    int porta;
+    char ip[30];
+    struct node * prox;
+};
+struct node *pilhaptr;
 
 
-int main(int argc, char *argv[]){
+int main(){
+    pthread_t th[30];
+    int porta, id, z,i, ti;
+    char ip[30];
+    MYSQL *conn;
+    MYSQL_RES *res;
+    MYSQL_ROW row;
 
+    char str[] = "SELECT id, ip, porta FROM ips WHERE checado IS NULL ORDER BY rand() ASC LIMIT 3000";
 
-        pthread_t th[THREADS];
-        MYSQL *conn;
-        MYSQL_RES *res;
-        MYSQL_ROW row;
-        int z,i;
-        char str[500];
-        char ip[30];
-        char lista[20000][30];
-        void *buffer[queue_s];
-        queue_t queue = QUEUE_INITIALIZER(buffer);
+    conn = mysql_init(NULL);
+    if (!mysql_real_connect(conn, server, user, password, database, 0, NULL, 0)) {
+        fprintf(stderr, "%s\n", mysql_error(conn));
+        exit(1);
+    }
 
+    if (mysql_query(conn, str)) {
+        fprintf(stderr, "%s\n", mysql_error(conn));
+        exit(1);
+    }
 
+    InitPilha();
 
+    res = mysql_use_result(conn);
+    while ((row = mysql_fetch_row(res)) != NULL){
+        empilha(row[1], atoi(row[2]), atoi(row[0]));
+    }
 
-        conn = mysql_init(NULL);
-
-        /* Connect to database */
-        if (!mysql_real_connect(conn, server, user, password,
-                                      database, 0, NULL, 0)) {
-                fprintf(stderr, "%s\n", mysql_error(conn));
-                exit(1);
-        }
-
-
-
-
-       sprintf(str, "SELECT id, ip, porta FROM ips WHERE checado IS NULL ORDER BY id ASC LIMIT 15000");
-
-        if (mysql_query(conn, str)) {
-                fprintf(stderr, "%s\n", mysql_error(conn));
-                exit(1);
-        }
-
-
-        res = mysql_use_result(conn);
-        i=0;
-        while ((row = mysql_fetch_row(res)) != NULL){
-                sprintf(lista[i],"%s:%s:%s", row[0], row[1], row[2]);
-                queue_enqueue(&queue, lista[i]);
-                i++;
-        }
-
-        mysql_free_result(res);
+    mysql_free_result(res);
         mysql_close(conn);
 
+    pthread_mutex_init(&lock, NULL);
+    pthread_cond_init(&cond, NULL);
 
-
-        if(queue_size(&queue) > 0 ){
-            for(z=0;z<THREADS;z++){
-                pthread_create(&th[z], NULL, worker, &queue);
-            }
-            for(z=0;z<THREADS;z++){
-                pthread_join(th[z], NULL);
-            }
+    if(pilhaVazia() != 1){
+        for(ti = 0; ti <MAX_TH; ti++){
+            pthread_create(&th[ti], NULL, worker, NULL);
         }
-        else{
-
+        for(ti = 0; ti <MAX_TH; ti++){
+            pthread_join(th[ti], NULL);
         }
+    }
 
+    sleep(3);
+    return 0;
+}
+
+
+void InitPilha(){
+    pilhaptr = NULL;
+}
+
+
+int pilhaVazia(){
+    if(pilhaptr == NULL) return 1;
+    else return 0;
+}
+
+
+void empilha(char ip[30], int porta, int id){
+        struct node *p;
+        p = malloc(sizeof(struct node));
+        p->id = id;
+    p->porta = porta;
+        strcpy(p->ip, ip);
+        p->prox = pilhaptr;
+        pilhaptr = p;
+}
+
+
+void desempilha(char *ip, int *porta, int *id){
+    struct node *p;
+    p = pilhaptr;
+    if(p==0) printf("Pilha vazia\n");
+    else{
+        pilhaptr = p->prox;
+        strcpy(ip, p->ip);
+        memcpy(porta, &p->porta, sizeof(int));
+        memcpy(id, &p->id, sizeof(int));
+    }
 }
 
 
 
+void * worker(){
+        char banner[2000], headers[4000], sql[7000], ip[30];
+        int porta, id;
+    MYSQL *sc;
+        sc = mysql_init(NULL);
+
+    if (!mysql_real_connect(sc, server, user, password, database, 0, NULL, 0)) {
+        fprintf(stderr, "%s\n", mysql_error(sc));
+        exit(1);
+    }
 
 
+    while(pilhaVazia() != 1){
+        pthread_mutex_lock(&lock);
+            desempilha(ip, &porta, &id);
+        pthread_cond_signal(&cond);
+        pthread_mutex_unlock(&lock);
 
+        head(ip, porta, banner, headers);
+        sprintf(sql, "UPDATE ips SET checado = 1, server = '%s', headers = '%s' WHERE id = %d", banner, headers, id);
 
-void head(char *host, int port, char *server){
-        int fd, n, res;
-        int sock;
-        char buffer2[BUFFER_SIZE];
-        char *ret;
-        int max;
-        fd_set sockets;
-
-        struct timeval tempo;
-
-        tempo.tv_sec = 5;
-        tempo.tv_usec = 0;
-
-        fd = socket_connect(host, port);
-        if(fd != -1){
-                write(fd, "HEAD / HTTP/1.0\r\n\r\n", strlen("HEAD / HTTP/1.0\r\n\r\n"));
-                for(;;){
-
-                        FD_ZERO(&sockets);
-                        FD_SET(fd , &sockets);
-
-                        //res = select(fd+1, &sockets, (fd_set *)0, (fd_set *)0, &tempo);
-                        res = select(fd+1, &sockets, &sockets, (fd_set *)0, &tempo);
-                        switch(res){
-                                case 0:
-                                        strncpy(server, "Desconhecido", 100);
-                                        shutdown(fd, SHUT_RDWR);
-                                        close(fd);
-                                        break;
-                                case -1:
-                                        strncpy(server, "Desconhecido", 100);
-                                        shutdown(fd, SHUT_RDWR);
-                                        close(fd);
-                                        break;
-                                default:
-                                        n = read(fd, buffer2, BUFFER_SIZE - 1);
-                                        parser2(buffer2);
-                                        strncpy(server, buffer2, 2000);
-                                        shutdown(fd, SHUT_RDWR);
-                                        close(fd);
-                                        break;
-                        }
-                        break;
-                        }
-                }
+                if (mysql_query(sc, sql)) {
+            fprintf(stderr, "%s\n", mysql_error(sc));
+printf("%s\n", sql);
+            exit(1);
         }
+    }
+    mysql_close(sc);
+}
 
 
 
+void head(char *host, int port, char *banner, char *headers){
+    int fd, n, res, sock, max = 1900, max2=3999;
+    char *ret, req[200], buffer2[BUFFER_SIZE];
 
+    fd_set sockets;
+    struct timeval tempo;
+    tempo.tv_sec = 5;
+    tempo.tv_usec = 0;
 
+    fd = socket_connect(host, port);
+    if(fd != -1){
 
+        sprintf(req, "HEAD / HTTP/1.1\r\nAccept: */*\r\nUser-Agent: Internet Fuzzer/0.1\r\nHost: %s:%d\r\nConnection: close\r\n\r\n", host,port);
+        write(fd, req, strlen(req));
+
+        for(;;){
+
+            FD_ZERO(&sockets);
+            FD_SET(fd , &sockets);
+            res = select(fd+1, &sockets, &sockets, (fd_set *)0, &tempo);
+            switch(res){
+                case 0:
+                    strncpy(banner, "Desconhecido", 12);
+                                banner[12] = '\0';
+                                strncpy(headers, "0", 1);
+                                headers[1]= '\0';
+                    break;
+                case -1:
+                    strncpy(banner, "Desconhecido", 12);
+                                banner[12] = '\0';
+                                strncpy(headers, "0", 1);
+                                headers[1] ='\0';
+                    break;
+                default:
+                    n = read(fd, buffer2, BUFFER_SIZE - 1);
+                    removeChar(buffer2, '\'');
+                            if(strlen(buffer2) > max2){
+                        strncpy(headers, buffer2, 3999);
+                                    headers[3999]= '\0';
+                                }
+                            else{
+                                    strncpy(headers, buffer2, strlen(buffer2));
+                                    headers[strlen(buffer2)] ='\0';
+                                }
+
+                    parser2(buffer2);
+                            if(strlen(buffer2) > max){
+                            strncpy(banner, buffer2, 1999);
+                                    banner[1999] ='\0';
+                                }
+                            else{
+                                    strncpy(banner, buffer2, strlen(buffer2));
+                                    banner[strlen(buffer2)]='\0';
+                                }
+                    break;
+            }
+        break;
+        }
+    shutdown(fd, SHUT_RDWR);
+    close(fd);
+    }
+}
 
 void parser2(char *string){
-        char *primeiro;
-        char *segundo;
-        char *ret=NULL;
-        int pos = 0, x = 0, y=0, tam =0;
+    char *primeiro, *segundo, *ret=NULL;
+    int pos = 0, x = 0, y=0, tam =0;
 
-        primeiro = strstr(string, "Server:");
-        segundo = strtok(primeiro, "\n");
-        if(primeiro != NULL && segundo != NULL){
-                strtok(segundo, "\r");
-                strncpy(string, segundo, BUFFER_SIZE);
+    primeiro = strstr(string, "Server:");
+    segundo = strtok(primeiro, "\n");
 
+    if(primeiro != NULL && segundo != NULL){
+        strtok(segundo, "\r");
+            if(strlen(segundo) > 1999){
+                strncpy(string, segundo, 1999);
+                string[1999] = '\0';
         }
-        else{
-                strncpy(string, "Desconhecido", 15);
-        }
+            else{
+                strncpy(string, segundo, strlen(segundo));
+            string[strlen(segundo)] = '\0';
+            }
+    }
+    else{
+        strncpy(string, "Desconhecido", 15);
+    }
 }
-
-
-
-
-
-
-
-
-
-
-void *worker(void *arg){
-        char *server;
-        char *line;
-        int port;
-        char sql[1000];
-        char id[11];
-        MYSQL *conn;
-        MYSQL_RES *res;
-        MYSQL_ROW row;
-        conn = mysql_init(NULL);
-
-      if (!mysql_real_connect(conn, server, user, password,
-                                      database, 0, NULL, 0)) {
-                fprintf(stderr, "%s\n", mysql_error(conn));
-                exit(1);
-        }
-
-
-
-        server = (char *)malloc(2000 * sizeof(char));
-//      line = (char *)malloc(40 * sizeof(char));
-
-        while(queue_size(arg) > 0){
-            //printf("falta: %d\n", queue_size(arg));
-            line = queue_dequeue(arg);
-            port = split(line, id);
-            head(line, port, server);
-
-            if(strlen(line) > 6){
-                        printf("%s:%d %s\n", line, port, server);
-                        sprintf(sql, "UPDATE ips SET checado = 1, server = '%s' WHERE id = %d", server, atoi(id));
-                        if (mysql_query(conn, sql)) {
-                                fprintf(stderr, "%s\n", mysql_error(conn));
-                                exit(1);
-                        }
-
-                }
-
-
-        }
-        mysql_close(conn);
-}
-
-
-
-
-void grava(char arquivo[], char ip[], int porta, char server[]){
-        FILE *fp;
-
-        if((fp = fopen(arquivo, "a+")) != NULL){
-                fprintf(fp, "http://%s:%d/|%s\n", ip, porta, server);
-                if(fclose(fp)){
-                        printf("Erro ao fechar %s\n", arquivo);
-                }
-        }
-}
-
-
-
-
-
-
-
-
-
-
 
 
 int socket_connect(char *host, in_port_t port){
-        struct hostent *hp;
-        struct sockaddr_in addr;
-        struct timeval tv;
-        int on = 1, sock;
-        if((hp = gethostbyname(host)) == NULL){
-                return -1;
+    struct hostent *hp;
+    struct sockaddr_in addr;
+    struct timeval tv;
+    int on = 1, sock;
+    if((hp = gethostbyname(host)) == NULL){
+        return -1;
+    }
+    else {
+        bcopy(hp->h_addr, &addr.sin_addr, hp->h_length);
+        addr.sin_port = htons(port);
+        addr.sin_family = AF_INET;
+        sock = socket(PF_INET, SOCK_STREAM, 0);
+        tv.tv_sec = 5;
+        tv.tv_usec = 0;
+        setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, (char *)&tv, sizeof(tv));
+        setsockopt(sock, SOL_SOCKET, SO_SNDTIMEO, (char *)&tv, sizeof(tv));
+        if(sock == -1){
+            return -1;
         }
-        else {
-                bcopy(hp->h_addr, &addr.sin_addr, hp->h_length);
-                addr.sin_port = htons(port);
-                addr.sin_family = AF_INET;
-                sock = socket(PF_INET, SOCK_STREAM, 0);
-//              fcntl(sock, F_SETFL, O_NONBLOCK);
-                tv.tv_sec = 5;
-                tv.tv_usec = 0;
-                setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, (char *)&tv, sizeof(tv));
-                setsockopt(sock, SOL_SOCKET, SO_SNDTIMEO, (char *)&tv, sizeof(tv));
-                if(sock == -1){
-                        return -1;
-                }
-                if(connect(sock, (struct sockaddr *)&addr, sizeof(struct sockaddr_in)) == -1){
-                        return -1;
-                }
-                return sock;
+        if(connect(sock, (struct sockaddr *)&addr, sizeof(struct sockaddr_in)) == -1){
+            return -1;
         }
+        return sock;
+    }
 }
 
 
-
-
-
-
-int split(char *str, char *id){
-        int x;
-        int i;
-        char ip[30];
-        char porta[7];
-
-        for(x=0;str[x] != ':'; x++){
-                id[x] = str[x];
+void removeChar(char * str, char charToRemmove){
+    int i, j;
+    int len = strlen(str);
+    for(i=0; i<len; i++)
+    {
+        if(str[i] == charToRemmove)
+        {
+            for(j=i; j<len; j++)
+            {
+                str[j] = str[j+1];
+            }
+            len--;
+            i--;
         }
-
-        id[++x] = '\0';
-
-        i=0;
-        for(x; str[x] != ':';x++){
-                ip[i] = str[x];
-                i++;
-        }
-        ip[i] = '\0';
-
-        i=0;
-        for(++x; x<strlen(str);x++){
-                porta[i] = str[x];
-                i++;
-        }
-
-        porta[i] = '\0';
-        strcpy(str, ip);
-
-        return atoi(porta);
+    }
 }
